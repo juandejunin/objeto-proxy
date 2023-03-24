c0b1764cc06f0490f826a28309e4f761aac0ab1c
juan75amieva <juan75amieva@gmail.com>
2023-03-24 16:19:19 +0100

    feat: modified

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-const input1 = document.getElementById('input-1')
-const input2 = document.getElementById('input-2')
+const valor1 = document.getElementById('input-1')
+const valor2 = document.getElementById('input-2')
 
 const sumar = document.getElementById('sumar')
 const restar = document.getElementById('restar')
@@ -17,22 +17,39 @@ dividir.addEventListener('click', ejecutarOperacion)
 //Escribir un proxy que contenga el acceso a las propiedades de un objeto que contenga la informacion de 
 //una operacion  matematica
 
+const plantilla = { valor1: null, valor2: null, operador: null }
+//los proxy incluyen un objeto que se llama  handler, los handler contienen funciones trampa que nos permiten
+//controlar el acceso a las propiedades de un objeto
+const handler = {
+    get:(target,key)=>{
+        if(key === 'valor1'){
+            return input1.value === ''? 0 : parseInt(input1.value)
+        }
+        if(key === 'valor2'){
+            return input2.value === ''? 0 : parseInt(input2.value)
+        }
+        return target[key]
+    }
+}
+
+const proxy = new Proxy(plantilla,handler)
+
 function calcularResultado() {
     let total
-    switch (operador) {
+    switch (proxy.operador) {
         case '+':
-            total = valor1 + valor2
+            total = proxy.valor1 + proxy.valor2
             break;
         case '*':
-            total = valor1 * valor2
+            total = proxy.valor1 * proxy.valor2
             break;
 
         case '-':
-            total = valor1 - valor2
+            total = proxy.valor1 - proxy.valor2
             break;
 
         case '/':
-            total = valor1 / valor2
+            total = proxy.valor1 / proxy.valor2
             break;
         default:
             break;
@@ -42,6 +59,8 @@ function calcularResultado() {
 }
 
 
-function ejecutarOperacion(evento){
+function ejecutarOperacion(evento) {
+
+    proxy.operador= evento.target.innerText
     calcularResultado()
 }
